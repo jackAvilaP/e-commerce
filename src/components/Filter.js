a48108cc2a523af72edf72be09ac649b2bdb2130
jackAvilaP <arenca44@gmail.com>
2022-06-18 05:06:47 -0500

    cambios en filter

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,16 +1,43 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import '../styles/Filter.css';
+import axios from 'axios';
+
+/*categorys.map((category) =>(
+  <li>
+    {category.name}
+  </li>
+))*/
 
 const Filter = () => {
+  const [categorys, setCategorys] = useState({})
+
+  useEffect(() => {
+    axios
+      .get(`https://ecommerce-api-react.herokuapp.com/api/v1/products/categories`)
+      .then((res) => setCategorys(res.data.data.categories));
+  }, [])
+ 
   return (
     <div className='accordion'>
-        <div>
-          <h3>Price</h3>
-        </div>
-        <div>
-          <input type='number' placeholder='Form'/>
-          <input type='number' placeholder='To'/> 
-        </div>
-        <button>filter price</button>
+      <details>
+        <summary>Price</summary>
+        <section>
+          <div>
+            <input type='number' placeholder='Form'/>
+            <input type='number' placeholder='To'/> 
+          </div>
+          <button>filter price</button>
+        </section>
+      </details>
+      <details>
+        <summary>Category</summary>
+        <section>
+          {
+
+          }
+        </section>
+      </details>
     </div>
   )
 }
