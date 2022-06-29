cec00b60933014de6f76ce8164d28c0ffedc3062
jackAvilaP <arenca44@gmail.com>
2022-06-28 19:43:09 -0500

    changes in ProductDetails and style

--- src/pages/ProductDetails.js
+++ src/pages/ProductDetails.js
@@ -1,38 +1,65 @@
-import React, { useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import "../styles/ProductDetails.css";
 
 import { useParams } from "react-router-dom";
 
 import axios from "axios";
 import SpinnerLoading from "../components/SpinnerLoading";
-
+import { useDispatch, useSelector } from "react-redux";
+import { categorysId } from "../store/slices/products.slice";
+import { Cards } from "../components";
 
 const ProductDetails = () => {
-  const [store, setStore] = useState([])
+  const dispatch = useDispatch();
+  const products = useSelector((state) => state.product);
+  const [store, setStore] = useState([]);
+  const [categoryid, setId] = useState({});
   const { id } = useParams();
 
   useEffect(() => {
     const odtenerProducto = async () => {
-      const result = await axios.get(`https://ecommerce-api-react.herokuapp.com/api/v1/products`)
-      
-        setStore( result.data.data.products.find(
-          (newProduct) => newProduct.id === Number(id)
-        ))
+      await axios
+        .get(`https://ecommerce-api-react.herokuapp.com/api/v1/products`)
+        .then((res) => {
+          const newSearch = res.data.data.products.find(
+            (newProduct) => newProduct.id === Number(id)
+          );
+          setStore(newSearch);
+          dispatch(categorysId(Number(newSearch.category.id)));
+        });
     };
-    odtenerProducto()
-  }, [id]);
- 
+    odtenerProducto();
+  }, [id, dispatch]);
+  console.log(store);
   return (
     <div className="container-details">
       <section id="flexbox-direction-img">
-
-        {
-          store.length === 0 ? <SpinnerLoading/> : 
+        <div>
+          {store.length === 0 ? (
+            <SpinnerLoading />
+          ) : (
             <div className="img-details">
-              <img  src={store.productImgs[0]} alt="product-img" />
+              <div className="container-flex-info">
+                <div className="containerImg-big">
+                  <img
+                    src={store.productImgs[0]}
+                    alt="product-img"
+                    id="img-big"
+                  />
+                </div>
+                <div className="product-info">
+                  <h2>{store.title}</h2>
+                  <p>{store.description}</p>
+                </div>
+              </div>
+              <div className="card-category">
+                {products.map((product) => (
+                  <Cards product={product} key={id + product.id} />
+                ))}
+              </div>
             </div>
-        }
-        
+          )}
+        </div>
       </section>
     </div>
   );
